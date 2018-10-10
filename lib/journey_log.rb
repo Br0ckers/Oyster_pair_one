29f9f8ca82e4d1ee015ae63d84697d550d09e750
Br0ckers <neil.brockman@yahoo.co.uk>
2018-10-10 17:23:36 +0100

    Chapter started

--- lib/journey_log.rb
+++ lib/journey_log.rb
@@ -1,4 +1,25 @@
 class Journey_log
-  def initialize
+  attr_reader :previous_journey
+
+  def
+    initialize
+      @previous_journey = {}
+  end
+
+  def start
+    @previous_journey = {:entry_station => "oldstreet"}
+  end
+
+  def current_journey
+    @previous_journey
+  end
+
+  def finish
+    @previous_journey[:exit_station] = "barbican"
+  end
+
+  def all_journeys
+    puts @previous_journey
+    return "start #{@previous_journey[:entry_station]} end #{@previous_journey[:exit_station]}"
   end
 end
--- spec/journey_log_spec.rb
+++ spec/journey_log_spec.rb
@@ -3,12 +3,29 @@ require 'journey_log'
 describe Journey_log do
   describe 'start' do
     it 'starts a journey' do
-      # journey = Journey.new
-      # journey.card.top_up(30)
-      # station = Station.new
-      # journey.touch_in(station, journey.card.balance)
-      # expect(subject.start).not_to eq nil
+      subject.start
+      expect(subject.previous_journey).not_to be_empty
     end
+  end
+  describe 'current journey' do
+    it 'shows current journey' do
+      subject.start
+      expect(subject.current_journey).not_to eq nil
+    end
+  end
+  describe 'end' do
+    it 'ends a journey' do
+      subject.start
+      subject.finish
+      expect(subject.previous_journey[:exit_station]).not_to eq nil
 
+    end
+  end
+  describe 'All journeys' do
+    it 'prints all jorneys' do
+      subject.start
+      subject.finish
+      expect(subject.all_journeys).to eq "start oldstreet end barbican"
+    end
   end
 end
